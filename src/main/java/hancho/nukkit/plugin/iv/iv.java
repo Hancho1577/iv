005a1b999fb6becdc3593000690de96f15c7a0fa
Hancho1577 <vojougae35@gmail.com>
2020-07-19 23:35:44 +0700

    Fix typo

--- src/main/java/hancho/nukkit/plugin/iv/iv.java
+++ src/main/java/hancho/nukkit/plugin/iv/iv.java
@@ -14,7 +14,7 @@ import cn.nukkit.item.Item;
 import cn.nukkit.plugin.PluginBase;
 
 public class iv extends PluginBase implements Listener {
-	public HashMap<String, Boolean> bvList = new HashMap<>();
+	public HashMap<String, Boolean> checkBlockQueue = new HashMap<>();
 
 	@Override
 	public void onEnable() {
@@ -28,8 +28,8 @@ public class iv extends PluginBase implements Listener {
 				sender.sendMessage("§l§f[ §c! §f] 인게임에서만 가능합니다.");
 				return true;
 			}
-			Item itemhand = ((Player) sender).getInventory().getItemInHand();
-			String code = itemhand.getId() + ":" + itemhand.getDamage();
+			Item itemInHand = ((Player) sender).getInventory().getItemInHand();
+			String code = itemInHand.getId() + ":" + itemInHand.getDamage();
 			sender.sendMessage("§l§f[ §6! §f] 아이템 코드 : " + code);
 			return true;
 		} else if (cmd.getName().equals("bv")) {
@@ -37,22 +37,21 @@ public class iv extends PluginBase implements Listener {
 				sender.sendMessage("§l§f[ §c! §f] 인게임에서만 가능합니다.");
 				return true;
 			}
-			if (this.bvList.containsKey(sender.getName())) {
-				this.bvList.remove(sender.getName());
+			if (this.checkBlockQueue.containsKey(sender.getName())) {
+				this.checkBlockQueue.remove(sender.getName());
 				sender.sendMessage("§l§f[ §6! §f] 블럭 아이템코드가 꺼졌습니다.");
-				return true;
 			} else {
-				this.bvList.put(sender.getName(), true);
+				this.checkBlockQueue.put(sender.getName(), true);
 				sender.sendMessage("§l§f[ §6! §f] 블럭을 터치하여 아이템코드를 확인할 수 있습니다.");
-				return true;
 			}
+			return true;
 		}
 		return true;
 	}
 
 	@EventHandler
 	public void onInteract(PlayerInteractEvent ev) {
-		if (this.bvList.containsKey(ev.getPlayer().getName())) {
+		if (this.checkBlockQueue.containsKey(ev.getPlayer().getName())) {
 			if (ev.getAction().equals(Action.RIGHT_CLICK_BLOCK))
 				ev.getPlayer().sendMessage("§l§f[ §6! §f] 블럭 정보 : " + ev.getBlock());
 		}
@@ -60,6 +59,6 @@ public class iv extends PluginBase implements Listener {
 
 	@EventHandler
 	public void onQuit(PlayerQuitEvent ev){
-		if (this.bvList.containsKey(ev.getPlayer().getName())) this.bvList.remove(ev.getPlayer().getName());
+		if (this.checkBlockQueue.containsKey(ev.getPlayer().getName())) this.checkBlockQueue.remove(ev.getPlayer().getName());
 	}
 }
